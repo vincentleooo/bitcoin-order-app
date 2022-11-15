0374f1a6e38879a46d565b09dba7cfbe00dc2e62
Vincent Leonardo <vincentleoooo@icloud.com>
2022-11-15 14:15:38 +0800

    lint, dast, sast

---
+++ scripts/zap-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker pull owasp/zap2docker-stable
+docker run -i owasp/zap2docker-stable zap-baseline.py -t "https://github.vinleonardo.com/bitcoin-order-app/" -l PASS > zap_baseline_report.html
+
+echo $? > /dev/null
